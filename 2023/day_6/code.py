0db24c21449b672b21578ae3992c990921343250
yukunchen113 <yukunchen113@gmail.com>
2023-12-06 00:46:01 -0500

    added day 6

--- 2023/day_6/code.py
+++ 2023/day_6/code.py
@@ -1,11 +1,5 @@
-import os
-from collections import defaultdict
 import regex as re
-from pprint import pprint
-from functools import reduce
-import numpy as np
 from advent import mark
-from advent.tools.map import convert_to_complex
 
 def parse_input(data_file):
     with open(data_file) as f:
